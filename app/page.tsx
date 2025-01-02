cf8b8a8868140bd60444e726151cb16915f3c5d5
israel mvono <israelmvono@gmail.com>
2025-01-02 10:53:07 +0300

    Add main page component

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import CountdownTimer from '../components/CountdownTimer';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <CountdownTimer />
+    </main>
+  );
+}
